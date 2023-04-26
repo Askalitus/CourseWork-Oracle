f968240c4321f332e5442f54de752c18e06f054e
Askalit <popugaq6@mail.ru>
2023-04-26 17:45:59 +0300

    save

---
+++ web/src/components/AdminBar.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="panel">
+        <p>Действие:</p>
+        <p class="clickable" @click="sortAll()">Создать пользователя</p>
+        <p class="clickable" @click="sortNew()">Статистика</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style scoped>
+.panel{
+    background-color: #323232;
+    padding: 1.57407407vh 0.885416667vw;
+    display: flex;
+    gap: 1.04166667vw;
+    border-radius: 0.78125vw;
+}
+p{
+    font-size: 0.78125vw;
+    font-weight: 700;
+    color: white;
+}
+.clickable{
+    font-size: 0.729166667vw;
+    font-weight: 500;
+}
+.clickable:hover{
+    color: #14FFEC;
+    cursor: pointer;
+}
+.selected{
+        color: #14FFEC;
+    }
+</style>
--- web/src/views/AdminView.vue
+++ web/src/views/AdminView.vue
@@ -1,13 +1,15 @@
 <template>
     <div>
-
+        <AdminBar />
     </div>
 </template>
 
 <script>
+import AdminBar from '../components/AdminBar.vue';
+
     export default {
-        
-    }
+    components: { AdminBar }
+}
 </script>
 
 <style scoped>
