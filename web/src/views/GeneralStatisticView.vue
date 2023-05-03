3296ea29bec092c82ed61ed3d5376ae222acb903
askalit <popugaq6@mail.ru>
2023-05-03 18:52:46 +0300

    add often applicant

--- web/src/views/GeneralStatisticView.vue
+++ web/src/views/GeneralStatisticView.vue
@@ -24,6 +24,10 @@
                     <p :class="{darkTitle: darkMode, lightTitle: lightMode}">Готовых заданий</p>
                     <p :class="{darkDesc: darkMode, lightDesc: lightMode}">{{ readyTasks }}</p>
                 </div>
+                <div class="item">
+                    <p :class="{darkTitle: darkMode, lightTitle: lightMode}">Частый заявитель</p>
+                    <p :class="{darkDesc: darkMode, lightDesc: lightMode}">{{ oftenApplicant.name + " " + oftenApplicant.surname  + " " + oftenApplicant.patronymic }}</p>
+                </div>
             </div>
         </div>
     </div>
@@ -35,7 +39,8 @@ import axios from 'axios';
         props: { darkMode: Boolean, lightMode: Boolean },
         data() {
             return {
-                tasks: []
+                tasks: [],
+                oftenApplicant: {}
             }
         },
         mounted() {
@@ -45,7 +50,38 @@ import axios from 'axios';
                 { "Access-Control-Allow-Origin": "http://localhost:3000" },
                 { withCredentials: true }
             )
-            .then((res) => (this.tasks = res.data));
+            .then((res) => {
+                this.tasks = res.data
+
+                let applicants = this.tasks.map(el => el.applicant)
+                let result = {};
+
+                for (let i = 0; i < applicants.length; ++i)
+                {
+                    var a = applicants[i];
+                    if (result[a] != undefined)
+                        ++result[a];
+                    else
+                        result[a] = 1;
+                }
+
+                let arr = Object.values(result)
+
+                let max = Math.max(...arr)
+
+                let resultApplicant = ''
+
+                for (let key in result){
+                    if(result[key] == max){
+                        resultApplicant = key
+                    }
+                }
+
+                axios
+                .get("http://localhost:3000/user/" + resultApplicant,
+                { withCredentials: true, "Access-Control-Allow-Origin": "http://localhost:3000" })
+                .then(res => this.oftenApplicant = res.data)
+            });
         },
         computed: {
             allTasks(){
